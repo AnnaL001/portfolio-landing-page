e4a2ecee63a267cbb9e85d5fd3f9e94b1fc9ac38
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-02-27 13:26:11 +0300

    Add custom styling and relevant links to contact section

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Initialize tooltip according to Bootstrap documentation
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
